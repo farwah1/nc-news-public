83fe602de7f8df1bdca71440a731587767aa3202
Farwah Kazmi <farwah@Farwahs-MacBook-Air.local>
2022-11-18 11:06:34 +0000

    prepare api for hosting

--- db/connection.js
+++ db/connection.js
@@ -5,10 +5,14 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const db = new Pool();
+const config =
+  ENV === "production" ? { connectionString: process.env.DATABASE_URL,} : {};
+
+
+const db = new Pool(config);
 
 module.exports = db;
